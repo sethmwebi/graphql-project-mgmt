2216a0bff75edb86dd73653478f5de3a2cc5a67e
Seth Mwebi <sethmwebi27@gmail.com>
2022-08-16 15:42:42 +0300

    finished up with graphql & apollo-client

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
 const express = require("express");
-const colors = require("colors")
-require("dotenv").config()
-const { graphqlHTTP} = require("express-graphql");
-const schema = require("./schema/schema")
-const connectDB = require("./config/db")
+const colors = require("colors");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Connect to db
-connectDB()
+connectDB();
 
-app.use("/graphql", graphqlHTTP({
-	schema,
-	graphiql: process.env.NODE_ENV === "development"
-}))
+app.use(cors())
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema,
+		graphiql: process.env.NODE_ENV === "development",
+	})
+);
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`)
-})
+	console.log(`Server running on port ${port}`);
+});
